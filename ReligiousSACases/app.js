d5476d2359533dd434ce824bdfb2f26759d35b72
Casey Alvarado <alvarado.casey@gmail.com>
2016-03-11 13:41:46 -0500

    compiled everything

---
+++ ReligiousSACases/app.js
@@ -0,0 +1,20 @@
+var exphbs = require('express-handlebars');
+
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var express = require('express');
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+    res.sendfile('./views/index.html');
+});
+app.listen(2000);
